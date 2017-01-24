d9676faa053c734eee742839d2a1c41583292db0
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2017-01-23 22:44:56 -0500

    Cree un controlador para la camara de la escena

---
+++ Assets/Scripts/Camara/CameraCtrl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+
+    public Transform target;            // The position that that camera will be following.
+    public float smooth = 3f;        // The speed with which the camera will be following.
+    public float rotationSmooth = 4f;
+
+    private Vector3 offset;                     // The initial offset from the target.
+    private Quaternion rot;
+    private bool rotate;
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+        rotate = false;
+
+        rot = transform.localRotation;
+    }
+
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smooth * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.Z))
+        {
+            Vector3 eul = rot.eulerAngles;
+            rot = Quaternion.Euler(eul.x, eul.y + 2.5f, eul.z);
+            rotate = true;
+        }
+        if(Input.GetKey(KeyCode.X))
+        {
+            Vector3 eul = rot.eulerAngles;
+            rot = Quaternion.Euler(eul.x, eul.y - 2.5f, eul.z);
+            rotate = true;
+        }
+
+        if (rotate)
+        {
+            float delta = rot.eulerAngles.y;
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, rot, rotationSmooth * Time.deltaTime);
+            delta -= transform.localEulerAngles.y;
+            if(Mathf.Abs(delta)<0.2f)
+            {
+                rotate = false;
+                transform.localRotation = rot;
+            }
+        }
+    }
+}
